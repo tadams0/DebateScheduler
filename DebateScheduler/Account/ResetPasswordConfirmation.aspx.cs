24334b14e44762e1c5887a03ba161e8e241e5e07
Taylor <Taylor@TAYLOR-LAPTOP>
2016-10-06 21:17:56 -0400

    Add project files.

---
+++ DebateScheduler/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DebateScheduler.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DebateScheduler/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DebateScheduler.Startup))]
+namespace DebateScheduler
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
